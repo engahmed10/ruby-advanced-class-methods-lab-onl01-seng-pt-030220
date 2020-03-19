44a82d5fe7c8c63997d5643963638e76cea71399
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-19 08:53:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
         }
    end
    def self.new_from_filename(filename)
-      puts    filename.split('-')
+          filename.split('-').map
     #  puts     artistname=filename[0]
     #puts    songname = filename[1].split(".")
 
