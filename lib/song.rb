673c25925f0d588fab46490dffc57e86a50e7ff4
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-19 06:54:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
-  @@all = []
+  @@all =[]
+
 
   def self.all
     @@all
@@ -9,5 +10,38 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+   def self.new_by_name(name)
+       song = self.new
+       song.name=name
+       song
+   end
+
+   def self.create_by_name(name)
+     song = self.new
+     song.name=name
+     song.save
+     song
+   end
+   def self.find_by_name(name)
+      @@all.find do |song|
+        song.name == name
+      end
+   end
+
+   def self.find_or_create_by_name(name)
+
+    if self.find_by_name(name)
+        @@all[0]
+    else
+      self.create_by_name(name)
+    end
+
+   end
 
 end
