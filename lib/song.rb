d89e3024dcfaca19d79327ee3e50c5f7af70e299
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-19 09:36:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,9 +60,7 @@ class Song
    end
 
   def self.create_from_filename(filename)
-         self.new_from_filename(filename)
-         save
-
+         self.new_from_filename(filename).save
 
   end
 
